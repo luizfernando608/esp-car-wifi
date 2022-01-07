167f491cd5a32e8143f929e37166c83bac6db583
Luiz Fernando Souza Luz <70658918+luizfernando608@users.noreply.github.com>
2022-01-07 15:10:32 -0300

    Opencv functions

--- Opencv_simple_test/opencv-loading-image.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-#Carregando uma imagem no opencv
-img = cv2.imread("download.png", cv2.IMREAD_COLOR)
-cv2.namedWindow("Hello World")
-cv2.imshow("Hello World", img)
-cv2.waitKey()
-
- 
